96956927bf2dfb55a9f06366eae28fdc82f27d63
Simen Haugerud Granlund <simen@hgranlund.com>
2017-04-19 14:48:59 +0200

    My first private ai

--- ai.js
+++ ai.js
@@ -1,16 +1,28 @@
 var Chess = require('./modules/chess-extended.js').Chess;
+var minmax = require('./modules/minmax.js');
+var rndJesus = require('./sample-players/rnd-jesus.js');
+var endgamer = require('./sample-players/endgamer.js');
+var minmaxer = require('./sample-players/minmaxer.js');
+exports.name = 'FaceOfBoe - ' + Math.floor(Math.random() * 1000);
 
-// var rndJesus = require('./sample-players/rnd-jesus.js');
-// var endgamer = require('./sample-players/endgamer.js');
-// var minmaxer = require('./sample-players/minmaxer.js');
 
-exports.name = 'Tørrfisk - ' + Math.floor(Math.random() * 1000);
+var pieceValues = {
+    'p': 1,
+    'n': 3,
+    'b': 3,
+    'r': 5,
+    'q': 9,
+    'k': 100,
+};
 
-// rnd-jesus.js
+var sumPieceValues = function (pieces) {
+    return pieces.reduce((a, b) => a + b, 0);
+};
+
 exports.move = function (board, doMove) {
-    var chess = new Chess(board),
-        moves = chess.moves(),
-        move = moves[Math.floor(Math.random() * moves.length)];
-
-    doMove(move);
+    minmax.move(board, doMove, function (chess) {
+        var black = sumPieceValues(chess.pieces('b').map((p) => pieceValues[p.type]));
+        var white = sumPieceValues(chess.pieces('w').map((p) => pieceValues[p.type]));
+        return (chess.turn() == 'w') ? white - black : black - white;
+    }, 10);
 };
