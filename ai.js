9c792c9bdf12dcf41a1416c76b47c3e01a921807
Simen Haugerud Granlund <simen@hgranlund.com>
2018-06-18 23:18:57 +0200

    Added castling bonus

--- ai.js
+++ ai.js
@@ -6,23 +6,25 @@ var Chess = require('./modules/chess-extended.js').Chess,
 var sum = (v1, v2) => {
         return v1 + v2;
     },
-    mapSquareToArrayPosition = function (square) {
+
+    getPositionScoreFromSquare = (table, square) => {
         if (!square || square.length !== 2) {
             console.log("error in mapSquareToArrayPosition")
             return 0;
         }
         square = square.toLowerCase();
-
-        return constants.squareLatterToInt[square[0]] + Math.abs(8 - parseInt(square[1])) * 8;
+        var x = constants.squareLatterToInt[square[0]],
+            y = 8 - parseInt(square[1]);
+        return table[y][x];
     },
-    piecePositionValue = function (piece) {
+
+    piecePositionValue = (piece) => {
         var positionScore = 0,
             pieceSquareTables = constants.pieceSquareTables,
-            c = piece.type.toLowerCase();
-
-        if (constants.pieceSquareTables.hasOwnProperty(piece.type)) {
-            var squareTable = (piece.color === 'w') ? pieceSquareTables[piece.type] : pieceSquareTables[piece.type].reverse();
-            positionScore = squareTable[mapSquareToArrayPosition(piece.square)];
+            type = piece.type.toLowerCase();
+        if (constants.pieceSquareTables.hasOwnProperty(type)) {
+            var squareTable = (piece.color === 'w') ? pieceSquareTables[type] : pieceSquareTables[type].slice().reverse();
+            positionScore = getPositionScoreFromSquare(squareTable, piece.square);
         }
         return positionScore;
     },
@@ -34,6 +36,7 @@ var sum = (v1, v2) => {
             })
             .reduce(sum, 0);
     },
+
     valueMoves = (chess) => {
         var score = chess.moves({
                 verbose: true
@@ -52,42 +55,72 @@ var sum = (v1, v2) => {
             .reduce(sum, 0);
         return score;
     },
+
     positionScore = (chess) => {
-        var scorePlayer = chess.pieces('w')
+        var white = chess.pieces('w')
             .map(p => {
                 return piecePositionValue(p)
             })
             .reduce(sum, 0);
-        var scoreOther = chess.pieces('b')
+        var black = chess.pieces('b')
             .map(p => {
                 return piecePositionValue(p)
             })
             .reduce(sum, 0);
-        return scorePlayer - scoreOther;
+        return white - black;
     },
+
+    castlingScoreColor = (chess, color) => {
+        var k = chess.pieces(color).find((p) => {
+                return p.type == constants.KING
+            }),
+            r1 = chess.pieces(color).reverse().find((p) => {
+                return p.type == constants.ROOK
+            }),
+            r2 = chess.pieces(color).find((p) => {
+                return p != r1 && p.type == constants.ROOK
+            });
+        if (![k, r1, r2].every((p) => {
+                return p && ['1', '8'].includes(p.square[1])
+            })) return 0;
+        var krr = [k, r1, r2].map(p => {
+            return constants.squareLatterToInt[p.square[0]]
+        })
+        if ((krr[0] > krr[1] && krr[0] > krr[2]) || (krr[0] < krr[1] && krr[0] < krr[2])) {
+            return 25;
+        }
+        return 0;
+    },
+
+    castlingScore = (chess) => {
+        return castlingScoreColor(chess, 'w') - castlingScoreColor(chess, 'b')
+    },
+
     materialScore = (chess) => {
         return (valuePieces(chess, 'w') - valuePieces(chess, 'b')) * 1000;
     },
+
     blackOrWhiteSign = chess => {
         return chess.turn() === 'w' ? 1 : -1
-    }
+    },
 
-var score = (chess) => {
-    var score = materialScore(chess);
-    if (chess.in_check()) score -= 5 * blackOrWhiteSign(chess);
-    if (chess.in_checkmate()) score -= 1000000 * blackOrWhiteSign(chess);
-    score += valueMoves(chess) * blackOrWhiteSign(chess);
-    score += positionScore(chess);
-    score += Math.random();
-    return score;
-}
+    score = chess => {
+        var score = materialScore(chess);
+        if (chess.in_check()) score -= 5 * blackOrWhiteSign(chess);
+        if (chess.in_checkmate()) score -= 1000000 * blackOrWhiteSign(chess);
+        //score += valueMoves(chess) * blackOrWhiteSign(chess);
+        score += positionScore(chess);
+        score += castlingScore(chess);
+        score += Math.random();
+        return score;
+    },
 
-move = function (board) {
-    var chess = new Chess(board);
-    if (chess.numberOfPieces() <= 5) return endgame.move(board);
+    move = (board) => {
+        var chess = new Chess(board);
+        if (chess.numberOfPieces() <= 5) return endgame.move(board);
 
-    return minmax.move(board, 2, score);
-};
+        return minmax.move(board, 2, score);
+    };
 
 exports.score = score;
 exports.move = move;
--- test/ai-test.js
+++ test/ai-test.js
@@ -3,30 +3,40 @@ var should = require('chai').should(),
     ai = require('./../ai.js');
 
 describe('When the ai is asked to generate a move', () => {
-    
     describe(', and we are at the starting position', () => {
         var chess = new Chess();
         var generatedMove;
         before('Make the ai generate a move', () => {
+            chess = new Chess();
             generatedMove = ai.move(chess.fen())
         });
-        
+
         it('should generate a move in a string representation', () => {
             generatedMove.should.be.a('string');
         });
-        
+
         it('should generate a legal move', () => {
             chess.moves().should.include(generatedMove);
         });
 
         it('should generate one of the best moves', () => {
-            generatedMove.should.be.oneOf(['e4', 'd4'])
+            generatedMove.should.be.oneOf(['e4', 'd4', 'Nc3', 'Nf3'])
         });
+        describe('And white has done his move', () => {
+            before('Make the ai generate a move', () => {
+                var chess = new Chess();                
+                chess.move('e4');
+                generatedMove = ai.move(chess.fen())
+            });
+            it('should generate one of the best moves', () => {
+                generatedMove.should.be.oneOf(['e6', 'd6', 'Nc6', 'Nf6'])
+            });
+        })
     })
-    
-    describe(', and the board is one move from checkmate', () =>  {
+
+    describe(', and the board is one move from checkmate', () => {
         var chess = new Chess('1k6/5R2/4Q3/8/8/8/4K1PP/8 w - - 1 2');
-        var generatedMove; 
+        var generatedMove;
 
         before('Make the ai generate a move', () => {
             generatedMove = ai.move(chess.fen())
@@ -42,7 +52,7 @@ describe('When the ai is asked to generate a move', () => {
     })
 });
 
-describe('When the scoring function evaluets', ()=> {
+describe('When the scoring function evaluets', () => {
     describe(' and black is in checkmate', () => {
         var chess = new Chess('k1Q5/5R2/8/8/8/8/4K3/8 b - - 0 1');
         it('should give the board a really high value', () => {
@@ -58,14 +68,20 @@ describe('When the scoring function evaluets', ()=> {
 
     describe('a board that is better for white', () => {
         var chess = new Chess('1k6/5R2/4Q3/8/8/8/4K1PP/8 w - - 1 2');
-        it('should give a possitive score', ()=>  {
+        it('should give a possitive score', () => {
             ai.score(chess).should.be.greaterThan(0);
         })
     })
 
-    it('should evaluete a sentered pawn better', () => {
+    it('should evaluete a sentered pawn better (white)', () => {
         var centerPawn = new Chess('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1');
         var notCenteredPawn = new Chess();
         ai.score(centerPawn).should.be.greaterThan(ai.score(notCenteredPawn));
+    })
+
+    it('should evaluete a sentered pawn better (black)', () => {
+        var centerPawn = new Chess('rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+        var notCenteredPawn = new Chess();
+        ai.score(centerPawn).should.be.lessThan(ai.score(notCenteredPawn));
     })
 })
